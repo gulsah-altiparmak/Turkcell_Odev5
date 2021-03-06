23480ec0788dfffc6404ff2d4593f10b20af58ee
gulsah-altiparmak <gulsahaltiparmakk@gmail.com>
2021-03-06 20:00:35 +0300

    Service Operation Alamofire

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Turkcell_Odev5' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Turkcell_Odev5
+  pod 'Alamofire'
+  pod 'SDWebImage'
+end
---
+++ Turkcell_Odev5/Struct/Photos.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// MARK: - Photo
+struct Photo: Codable {
+    let albumID, id: Int
+    let title: String
+    let url, thumbnailURL: String
+
+    enum CodingKeys: String, CodingKey {
+        case albumID = "albumId"
+        case id, title, url
+        case thumbnailURL = "thumbnailUrl"
+    }
+}
+
+typealias Photoss = [Photo]
